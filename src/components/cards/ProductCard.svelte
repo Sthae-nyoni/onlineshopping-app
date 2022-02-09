ea3996c0c4fec6596be12fcfeb9774b70f337e14
Dumolwenkos <96240667+Dumolwenkos@users.noreply.github.com>
2022-02-09 12:02:20 +0200

    [feature]implemented base design of product card

---
+++ src/components/cards/ProductCard.svelte
@@ -0,0 +1,12 @@
+<div class="bg-orange-400 flex overflow-hidden rounded-md">
+    <img src="/static/download.jfif" alt="" class="w-full h-32">
+    <h2>{item}</h2>
+    <h2>{price}</h2>
+    <h3 class="flex-col">{description}</h3>
+</div>
+
+<script>
+    var item = "grocery";
+    var price = "$0.00";
+    var description = "Lorem ipsum and so on you know";
+</script>
